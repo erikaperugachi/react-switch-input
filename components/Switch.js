af143404be9801000ab04763d1879ba3badf0a4f
Erika Perugachi <erikaperugachi@gmail.com>
2018-01-09 12:20:07 -0500

    Add propTypes, defaultProps and onChange by default

--- components/Switch.js
+++ components/Switch.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './switch.css';
 
 class Switch extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      checked: false
+    };
+    this.handleOnChanged = this.handleOnChanged.bind(this);
   }
 
   render() {
     return (
-      <div className={"input-switch" + ( this.props.disabled ? " disabled" : "") + " style-switch-" + (this.props.style ? this.props.style : "one")}>
+      <div className={"input-switch" + ( this.props.disabled ? " disabled" : "") + " style-switch-" + this.props.theme}>
         { this.props.labelLeft
           ? <label htmlFor={this.props.name}>{this.props.labelLeft}</label>
           : null
@@ -17,9 +22,9 @@ class Switch extends Component {
           type="checkbox"
           id={this.props.name}
           name={this.props.name}
-          checked={this.props.checked}
+          checked={this.props.onChange ? this.props.checked : this.state.checked}
           disabled={this.props.disabled}
-          onChange={this.props.onChange}/>
+          onChange={this.props.onChange ? this.props.onChange : this.handleOnChanged}/>
         <label htmlFor={this.props.name}></label>
         { this.props.labelRight
           ? <label htmlFor={this.props.name}>{this.props.labelRight}</label>
@@ -28,6 +33,27 @@ class Switch extends Component {
       </div>
     );
   }
+
+  handleOnChanged(e) {
+    this.setState({
+      checked: e.target.checked
+    });
+  }
+
+}
+
+Switch.defaultProps = {
+  theme: "one"
+}
+
+Switch.propTypes = {
+  id: PropTypes.string,
+  disabled: PropTypes.bool,
+  theme: PropTypes.string,
+  name: PropTypes.string,
+  labelLeft: PropTypes.string,
+  onChange: PropTypes.func,
+  labelRight: PropTypes.string
 }
 
 export default Switch;
